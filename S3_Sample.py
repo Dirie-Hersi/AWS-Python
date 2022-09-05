ef84a3023a60ccadb32bc7f8158214e2c0bbd6a0
Dirie Hersi <dnhersi@gmail.com>
2022-09-05 22:59:42 +0000

    Fixed location and final print statement

--- S3_Sample.py
+++ S3_Sample.py
@@ -1,15 +1,15 @@
 import boto3
 
 aws_resource=boto3.resource("s3")
-bucket=aws_resource.Bucket("my-first-bucket-dhersi-us1")
+bucket=aws_resource.Bucket("my-first-bucket-dhersi-us1-2")
 
 
 response = bucket.create(
     ACL='public-read',
     CreateBucketConfiguration={
-        'LocationConstraint': 'us-east-1'
+        'LocationConstraint':'us-east-1'
     },
    
 )
 
-print(resource)
+print(response)
