badb3b8bfa06045fd04b630e5dc85bd53e557d36
Dirie Hersi <dnhersi@gmail.com>
2022-09-09 23:44:29 +0000

    Added count of vpcs

--- VPC/Describe_VPC.py
+++ VPC/Describe_VPC.py
@@ -5,5 +5,15 @@ import json
 client = boto3.client('ec2')
 
 response = client.describe_vpcs()
+totalvpcs = response["Vpcs"]
+
+print("The total number of VPCs is:", len(totalvpcs))
+
+for vpc in totalvpcs:
+    print(vpc["VpcId"])
+    
+print()
 
 print(json.dumps(response, indent=2, default=str))
+
+print()
