ce65b038dcd77f1a18c74c402a77e48567351294
Dirie Hersi <dnhersi@gmail.com>
2022-09-09 03:11:13 +0000

    finalized delete objects

--- S3/Delete_Objects_s3.py
+++ S3/Delete_Objects_s3.py
@@ -3,5 +3,12 @@ import boto3
 s3 = boto3.client("s3")
 
 s3.delete_object(Bucket="my-first-bucket-dhersi-us1-3", Key="file1.txt")
-
 print("File deleted")
+
+
+#Delete Multiple Objects
+#objects = s3.list_objects(Bucket = "my-first-bucket-dhersi-us1-3")["Contents"]
+#for object in objects:
+    #print(object["Key"])
+    #s3.delete_object(Bucket = "my-first-bucket-dhersi-us1-3", Key = object["Key"])
+    
