acb17f127fd9d26ab3488f62aade6eaad67c12aa
Dirie Hersi <dnhersi@gmail.com>
2022-09-10 03:22:51 +0000

    Final version

--- Ec2/Get_Ec2_Instance_ID.py
+++ Ec2/Get_Ec2_Instance_ID.py
@@ -1,10 +1,21 @@
 import boto3
-import json
 
+#Using both resource and client
 ec2_client = boto3.resource("ec2")
+ec2= boto3.client("ec2")
 
+
+#Printing total number of Ec2
+number = ec2.describe_instances()
+total_number = len(number["Reservations"])
+print(f"You currently have {total_number} Ec2 instances")
+
+print()
+
+#resource variable
 instances = ec2_client.instances.all()
 
+#loop to get ec2 data
 for instance in instances:
     print(f'EC2 instance {instance.id}" information:')
     print(f'Instance state: {instance.state["Name"]}')
