6dd8612355804745f652bc748a1312d6b1129ac8
Dirie Hersi <dnhersi@gmail.com>
2022-09-10 03:50:31 +0000

    Final ver

---
+++ Ec2/Terminate_Multiple_Instances.py
@@ -0,0 +1,8 @@
+
+   #Multiple terminations using a list
+   #import boto3
+    #ids = ['e1','e2','e3','e4','e5']
+    #ec2 = boto3.resource('ec2')
+    #ec2.instances.filter(InstanceIds = ids).terminate()
+    
+     
