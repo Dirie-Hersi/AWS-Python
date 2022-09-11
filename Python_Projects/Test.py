d1dc4f1320953eea71dc984b3457b16f075977b2
Dirie Hersi <dnhersi@gmail.com>
2022-09-11 03:53:05 +0000

    Added Write Items

--- Python_Projects/Test.py
+++ Python_Projects/Test.py
@@ -1,38 +1,18 @@
 import boto3
 
-#Invoke the resource
-my_dynamodb = boto3.resource('dynamodb')
-
-#Create the table
-table = my_dynamodb.create_table(
-    AttributeDefinitions=[
-        {
-            'AttributeName': 'Destination',
-            'AttributeType': 'S'
-        },
-        {
-            'AttributeName': 'Ranking',
-            'AttributeType': 'N'
-        },
-    ],
-    TableName='Top-Global-Tourist-Destinations',
-    KeySchema=[
-        {
-            'AttributeName': 'Destination',
-            'KeyType': 'HASH'
-        },
-        {
-            'AttributeName': 'Ranking',
-            'KeyType': 'RANGE'
-        },
-    ],
+dynamodb = boto3.resource(
+    'dynamodb',
+    aws_access_key_id='AKIAU27FDFMJCKHLBAH3',
+    aws_secret_access_key='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',
+    )
     
-    ProvisionedThroughput={
-        'ReadCapacityUnits': 10,
-        'WriteCapacityUnits': 10
-    },
-    
-)
+#Writing to DynamoDB
 
-#Printing table status
-print("Table Status:", table.table_status)
+with dynamodb.batch_writer() as batch:
+    batch.put_item(
+        Item={
+            'Destination': 'Auckland',
+            'Ranking': '1',
+           
+        }
+    )
