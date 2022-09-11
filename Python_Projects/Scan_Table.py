792dfec94927d053a8d2684d5fae071a0833ff6b
Dirie Hersi <dnhersi@gmail.com>
2022-09-11 05:27:07 +0000

    Updated scan

--- Python_Projects/Scan_Table.py
+++ Python_Projects/Scan_Table.py
@@ -1,7 +1,8 @@
-import boto3
-
-
-# Get the service resource.
-dynamodb = boto3.resource('dynamodb')
-
-response = dynamodb.scan()
+import boto3# Get the service resource.
+dynamodb = boto3.resource('dynamodb')#Getting the response and generating a scan
+table = dynamodb.Table('Top-Global-Tourist-Destinations')
+response = table.scan()
+data = response['Items']#Loop to print out items
+for item in data:
+    print(item)
+    print('-'*60)
